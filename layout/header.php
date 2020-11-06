56455945c2c540ba0dbca0358c6f184751e66341
Wiktor <wiktorwiese2000@gmail.com>
2020-11-06 22:35:38 +0100

    06.11.20 - InstalationPacket

---
+++ layout/header.php
@@ -0,0 +1,43 @@
+<header>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarExample01"
+        aria-controls="navbarExample01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarExample01">
+        <div class="navbar-brand">
+            <a>Hunter AG</a>
+        </div>
+        <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" aria-current="page" href="#">Strona Główna</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Produkty</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Złóż zamówienie</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Kontakt</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="./pages/login.php">Zaloguj się</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Navbar -->
+</header>
